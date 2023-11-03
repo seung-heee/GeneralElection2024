1d6c24a9ecb04a6b196cb22c0207cea674a36cec
seung-heee <tmdgmldi2595@naver.com>
2023-11-03 16:25:16 +0900

    Layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './Main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App Font_bombaram">
+        <Routes>
+          <Route path='/' element={<Main />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+const Main = () => {
+    return (
+        <div className='Main relative min-h-screen'>
+            <div className="absolute top-3 left-1 verticalLeft text-3xl">2023.11.20-11.22</div>
+            <div className="absolute bottom-48 right-1 verticalRight text-3xl">2023.11.20-11.22</div>
+            <div className='bottomText text-center bottom-2 absolute w-full'>제 40대 선거관리위원회</div>
+        </div>
+    )
+}
+export default Main;
