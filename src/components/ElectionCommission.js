d4c591a035498e366f15a7d2726b4cef43cd2923
seung-heee <tmdgmldi2595@naver.com>
2023-11-12 20:22:56 +0900

    lineShort

--- src/components/ElectionCommission.js
+++ src/components/ElectionCommission.js
@@ -9,7 +9,7 @@ const ElectionCommission = () => {
             <div className="mb-3 mt-4">
                 <div className="text-center"><img src={images.선관위} alt='선관위' className="w-14 mx-auto mb-1" /></div>
                 <p className="text-sm">제 40대 선거관리위원회</p>
-                <p className="text-sm"><Link to='#' className="flex items-center justify-center"><AiOutlineInstagram className="mr-1" /><span>Likelion_sku</span></Link></p>
+                <p className="text-sm"><Link to='https://www.instagram.com/sku_39th_renew/' className="flex items-center justify-center"><AiOutlineInstagram className="mr-1" /><span>sku_39th_renew</span></Link></p>
             </div>
 
             <div className="grid grid-cols-3 mb-16">
--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -2,11 +2,12 @@ import '../App.css';
 import { images } from '../utils/images';
 
 const PageTitle = ({ title, line }) => {
+    const LineShort = title === '미디어소프트웨어학과' || title === '글로벌경영기술대학' || title === '기독교교육상담학과' || title === '국제개발협력학과'
     return (
         <>
             {line ?
                 <>
-                    <div className='TItleIMG2 w-full h-20 flex items-center justify-center relative z-20'>
+                    <div className={`${LineShort ? 'TitleLineShort' : 'TItleIMG2'} w-full h-20 flex items-center justify-center relative z-20`}>
                         <div className="text-4xl text-center">
                             {title}
                         </div>
