39826a8d4f116bfb373bb459be447022c84ae903
minseo121 <kittse1217@gmail.com>
2025-02-07 21:22:48 +0900

    Fix : 총선거 만든이들 수정

--- src/components/ElectionCommission.js
+++ src/components/ElectionCommission.js
@@ -3,7 +3,25 @@ import { images } from '../utils/images';
 import Person from "./Person";
 import { AiOutlineInstagram } from 'react-icons/ai';
 
-const ElectionCommission = () => {
+export const ElectionCommission = () => {
+    return (
+        <div>
+            <div className="mb-3 mt-4">
+                <div className="text-center"><img src={images.선관위} alt='선관위' className="w-14 mx-auto mb-1" /></div>
+                <p className="text-sm">제40대 선거관리위원회</p>
+                <p className="text-sm"><Link to='https://www.instagram.com/sku_bisang/'  target="_blank" className="flex items-center justify-center"><AiOutlineInstagram className="mr-1" /><span>sku_bisang</span></Link></p>
+            </div>
+
+            <div className="grid grid-cols-2 mb-16">
+                <Person id={8} />
+                <Person id={9} />
+                <Person id={10} />
+            </div>
+        </div>
+    )
+}
+
+export const ByElectionCommission = () => {
     return (
         <div>
             <div className="mb-3 mt-4">
@@ -22,4 +40,3 @@ const ElectionCommission = () => {
     )
 }
 
-export default ElectionCommission;
